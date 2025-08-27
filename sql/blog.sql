b77bfd5e46090f4a055f690d33fbcbd6697f2d16
ManuhRodrigues <manuhrodrigues1905@gmail.com>
2025-08-27 11:57:14 -0300

    sql

---
+++ sql/blog.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS blog;
+CREATE DATABASE IF NOT EXISTS blog;
+USE blog;
+ CREATE TABLE usuario (
+id int NOT NULL AUTO_INCREMENT,
+nome varchar (50) NOT NULL,
+email varchar(255) NOT NULL,
+senha varchar (60) NOT NULL,
+data_criacao DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ativo TINYINT NOT NULL DEFAULT '0',
+adm TINYINT  NOT NULL DEFAULT '0',
+PRIMARY KEY (id)
+);
+CREATE TABLE post (
+id int NOT NULL AUTO_INCREMENT,
+titulo varchar (255) NOT NULL,
+texto text NOT NULL,
+usuario_id int NOT NULL,
+data_criacao DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+data_postagem DATETIME NOT NULL,
+PRIMARY KEY (id),
+KEY fk_post_usuario_idx (usuario_id),
+CONSTRAINT fk_post_usuario FOREIGN KEY (usuario_id) REFERENCES usuario (id)
+);
